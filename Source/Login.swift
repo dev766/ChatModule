797e00a8f64594f11d875575a98875d45d7c5e14
dev766 <akashpatil338@gmail.com>
2022-06-27 18:14:58 +0530

    create user methods added

--- Source/Login.swift
+++ Source/Login.swift
@@ -38,5 +38,21 @@ public class Login{
             }
         })
     }
+    
+    func CreateUserOnFireBase(_ email: String, password: String, completion: @escaping (_ Success: Bool, _ message: String) -> Void){
+        Auth.auth().createUser(withEmail: email, password: password, completion: { (authResult, error) in
+            if error != nil {
+                completion(false, "Error creating account")
+            } else {
+                guard let firebaseUID = authResult?.user.uid else{
+                    completion(false, "authResult?.user.uid not created")
+                    return
+                }
+                print(firebaseUID)
+                print("Successfully created account")
+
+            }
+        })
+    }
 
 }
