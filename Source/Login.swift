8d290eb18971c27c3ac93775eb98bf84816fefca
dev766 <akashpatil338@gmail.com>
2022-06-27 10:22:22 +0530

    added story board

--- Source/Login.swift
+++ Source/Login.swift
@@ -20,25 +20,13 @@ public class Login{
     
     public func signInToFirebase(with email: String,and password: String, onCompletion:@escaping ((Bool)->())) {
         Auth.auth().signIn(withEmail: email, password: password, completion: { (authResult, error) in
-            print("uuid is redy",authResult?.user.uid)
             if error != nil {
-
                 onCompletion(false)
             } else {
                 guard let firebaseUID = authResult?.user.uid else{
                     onCompletion(false)
                     return
                 }
-//                if Preferences.token == ""
-//                {
-////                    keychain.set(firebaseUID, forKey: "firebaseUID")
-//                }
-//                else
-//                {
-////                    keychain.set(firebaseUID, forKey: "firebaseUIDTemp")
-//                }
-
-//                onCompletion(true)
 
                 if let usr = authResult?.user {
                     Auth.auth().updateCurrentUser(usr) { (err) in
