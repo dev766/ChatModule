6827875079455c9f35894f19880872f013d84414
dev766 <akashpatil338@gmail.com>
2022-07-01 13:46:42 +0530

    create user

--- Source/HomeViewController.swift
+++ Source/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
@@ -17,7 +18,28 @@ class HomeViewController: UIViewController {
     
     @IBAction func GoAction(_ sender: UIButton) {
         print("go press")
+        CreateUserOnFireBase("akash@test.com", password: "122344") { Success, message in
+            if Success{
+                print("------------success")
+            }else{
+                print("------------error")
+            }
+        }
     }
-    
+    func CreateUserOnFireBase(_ email: String, password: String, completion: @escaping (_ Success: Bool, _ message: String) -> Void){
+        Auth.auth().createUser(withEmail: email, password: password, completion: { (authResult, error) in
+            if error != nil {
+                completion(false, "Error creating account")
+            } else {
+                guard let firebaseUID = authResult?.user.uid else{
+                    completion(false, "authResult?.user.uid not created")
+                    return
+                }
+                print(firebaseUID)
+                print("Successfully created account")
+
+            }
+        })
+    }
 
 }
